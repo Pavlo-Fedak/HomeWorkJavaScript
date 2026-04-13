34105badf3b0c2017442fe8652a0c03caac82306
Pavlo-Fedak <pashafedak1997@gmail.com>
2026-04-13 16:19:05 +0300

    Completed homework 1

---
+++ Lessons/HW_1/task_2/scripts/script.js
@@ -0,0 +1,8 @@
+let firstName = 'Pavlo';
+let middleName = 'Pavlovich';
+let lastName = 'Fedak';
+
+
+let person = firstName + ' ' + middleName + ' ' + lastName;
+
+console.log(person);
---
+++ Lessons/HW_1/task_4/scripts/script.js
@@ -0,0 +1,5 @@
+let firstName = prompt('Введи своє ім’я');
+let middleName = prompt('Введи по батькові');
+let age = prompt('Введи свій вік');
+
+console.log(firstName, middleName, age);
